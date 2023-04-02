cde43377efa35fac986006d6251a8b8df8b4115d
ytnkm <mikeflorenzhernandez9@gmail.com>
2023-04-03 00:37:28 +0800

    step 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,8 @@ import { ProductListComponent } from './product-list/product-list.component';
 
 const appRoutes: Routes = [
   { path: 'products', component: ProductListComponent },
-  { path: '/product/:id', component: ProductPageComponent }
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: '/product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' }
   
 ]; 
 @NgModule({
